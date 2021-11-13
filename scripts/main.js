5540de7bb89e9e4f4f49def4d7f514ac44a96e92
WumboPS <usharma9@my.bcit.ca>
2021-11-12 17:11:14 -0800

    Implemented card saving for new users

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,15 @@ function signOut() {
     alert("Sign Out Successfully from System");
 }
 
+// Keep to store all cards and user uid 
+var cards_lists = [];
+
 function insertName() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
         if (user) {
             // Do something for the current logged-in user here: 
-
+            cards_lists.push(user.uid);
             // This function will set the users name, i.e. Welcome Urjit
             set_user_name(user);
 
@@ -37,9 +40,11 @@ function set_user_name(user) {
         })
 }
 
-var cards_lists = [];
+
+// Keep
 var user_card_list = [];
 var card_documents = [];
+var current_highest_card = 0;
 
 async function get_documents(user_id) {
     const snapshot = await db.collection('users').doc(user_id).collection('cards').get()
@@ -50,30 +55,9 @@ async function get_documents(user_id) {
 }
 
 async function load_cards(user) {
-    var card_list = '';
-    var card_id = [];
-
-    // Get users uid
-    currentUser = db.collection("users").doc(user.uid);
-    currentUser.get()
-        .then(userDoc => {
-            card_list = userDoc.data().cards;
-            user_card_list.push(card_list);
-        })
-        .then(function () {
-            try {
-                card_id = card_list.split(/\s+/);
-            } catch (err) {
-                no_cards_exist();
-            }
-        })
-
-    cards_lists.push(user.uid);
-
     await get_documents(user.uid)
         .then(function () {
             card_documents.forEach(function (element) {
-
 
                 var due_date = element['due']
                 var title = element['title']
@@ -86,7 +70,7 @@ async function load_cards(user) {
 
             })
         })
-    
+
 }
 
 
@@ -157,6 +141,7 @@ function get_cardid(cardid) {
     }
 }
 
+
 async function save_new_info(card_id) {
     // Save the acutal new infromation
     actual_cardid = get_cardid(card_id)
@@ -170,6 +155,19 @@ async function save_new_info(card_id) {
         title: document.getElementById(actual_cardid + 'title').value,
     })
 }
+
+function get_highest_card() {
+    local_current_heighest_card = 0
+    if (card_documents == null) {
+        return current_highest_card = 0
+    } else {
+        card_documents.forEach(function (element) {
+            var card_id = element['card_id'];
+        })
+    }
+
+}
+
 
 function disable_card_form(obj, state) {
     parent_div1 = obj.parentElement;
@@ -199,28 +197,31 @@ function add_card() {
 
         db.collection("users").doc(cards_lists[0]).update({
             cards: user_card_list[0]
-        })
-    }
-    else {
-        //This meanns the user has no cards ...
-        last_element = cards_lists[cards_lists.length - 1];
-        found_card_number = last_element.match(/.{1,4}/g);
-        new_number = parseInt(found_card_number[1]);
-        new_number = new_number + 1;
-        new_card_id = 'card' + String(new_number);
-        console.log(new_card_id);
-    }
+        });
+    } else {           
+        cards_lists.push('card1');
+        db.collection('users').doc(cards_lists[0]).collection("cards").doc("card1").set({
+            title: 'Assignment Title',
+            description: 'Assignment Description',
+            due: '11/11/2021',
+            card_id: 'card1'
+        });
+        create_card_from_db('Assignment Title', 'Assignment Description', '11/11/2021', "card1");
+
+    }   
 
 }
 
 
 function no_cards_exist() {
-    document.getElementById("card_container").innerHTML = 'You currently have no cards saved';
+    if (cards_lists.length == 1) {
+        console.log(cards_lists);
+        document.getElementById("error_messages").innerHTML = 'You currently have no cards saved';
+    }
 }
+no_cards_exist();
 
 function select_date() {
-    console.log("touched")
-
     var date_input = $('input[name="date"]'); //our date input has the name "date"
     var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
     var options = {
